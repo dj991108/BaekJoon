03b56885513d4bdf2c7a4a0972cc4eb8498603ac
DongJun Kim <dj991108@naver.com>
2022-08-13 00:17:29 +0900

    add 2022/08/13

---
+++ src/lv_5/Lv5_4673.java
@@ -0,0 +1,37 @@
+package lv_5;
+
+public class Lv5_4673 {
+	
+	public static int d(int num) {
+		int sum = num;
+		
+		while(num !=0) {
+			sum += (num%10); // 1의 자리수
+			num = num/10; // 그 외
+		}
+		return sum;
+	}
+	
+	public static void check(int c[]) {
+		for(int i=1 ; i<=c.length; i++) {
+			int n = d(i);
+			
+			if(n < c.length) {
+				c[n] = -1;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		int[]c = new int[10001];
+		check(c);
+		
+		for(int j=1; j<10001 ; j++) {
+			
+			if(c[j] != -1) {
+				System.out.println(j);
+			}
+		}
+	}
+}
