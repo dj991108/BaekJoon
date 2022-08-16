e438899ea8df9624a3e35419649d97628bd1537a
DongJun Kim <dj991108@naver.com>
2022-08-16 10:00:00 +0900

    add 2022/08/17

--- src/lv_5/Lv5_1065.java
+++ src/lv_5/Lv5_1065.java
@@ -1,49 +1,38 @@
 package lv_5;
 
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Lv5_1065 {
 
-	private static LinkedList<Integer> stack = new LinkedList<>();
-	private static LinkedList<Integer> seq = new LinkedList<>();
-	static int count = 0;
-	
-	static boolean check(int i) {
-		int count_2 = 0;
-		while(i > 0) {
-			stack.push(i%10);
-			i /= 10;
-		}
-		while((!stack.isEmpty())){
-			seq.push(stack.pop());
-			count_2++;
-		}
-		
-		while((!seq.isEmpty())) {
-			try {
-				if(seq.pop()-seq.peek() != seq.pop()-seq.peek()) {
-					return false;
-				}
-			}catch(Exception e){
-				return true;
-			}
-		}	
-		return true;
-	}
-	
 	public static void main(String[] args) {
-		
 		Scanner s = new Scanner(System.in);
-		int num = s.nextInt();
-		for(int i=1 ; i<= num ; i++) {
-			if(check(i)) {
-				count++;
-			}
-			if(count == 104) {
-				System.out.println("!!"+i);
+		
+		System.out.print(arithmetic_sequence(s.nextInt()));
+
+		
+	}
+ 
+	public static int arithmetic_sequence(int num) {
+		int count = 0;
+ 
+		if (num < 100) {
+			return num;
+		}
+ 
+		else {
+			count = 99;
+ 
+			for (int i = 100; i <= num; i++) {
+				int hun = i / 100; 
+				int ten = (i / 10) % 10; 
+				int one = i % 10;
+ 
+				if ((hun - ten) == (ten - one)) { 
+					count++;
+				}
 			}
 		}
-		System.out.println(count);
+ 
+		return count;
 	}
 }
