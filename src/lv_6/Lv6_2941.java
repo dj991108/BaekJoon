0024c6a42caaa8b1c2c21f4350c1d33a6c4c8406
DongJun Kim <dj991108@naver.com>
2022-08-18 23:37:50 +0900

    add 2022/08/18

---
+++ src/lv_6/Lv6_2941.java
@@ -0,0 +1,29 @@
+package lv_6;
+
+import java.util.Scanner;
+
+public class Lv6_2941 {
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println(Croatia(s.next()));
+	}
+	
+	public static int Croatia(String alpha) {
+		
+		int count=0;
+		
+		if(alpha.contains("c=")) { count++; }
+		if(alpha.contains("c-")) { count++; }
+		if(alpha.contains("dz=")) { count++; }
+		if(alpha.contains("d-")) { count++; }
+		if(alpha.contains("lj")) { count++; }
+		if(alpha.contains("nj")) { count++; }
+		if(alpha.contains("s=")) { count++; }
+		if(alpha.contains("z=")) { count++; }
+		
+		return count;
+		
+	}
+
+}
