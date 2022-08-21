ab3e480d847e95437f2a06c0f971b9669aa4907a
DongJun Kim <dj991108@naver.com>
2022-08-21 19:20:11 +0900

    add 2022/08/21

--- src/lv_6/Lv6_2941.java
+++ src/lv_6/Lv6_2941.java
@@ -1,37 +1,71 @@
 package lv_6;
-
 import java.util.Scanner;
 
 public class Lv6_2941 {
 	
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		// System.out.println(Croatia(s.next()));
+		System.out.println(Croatia(s.next()));
 	}
 	
 	public static int Croatia(String alpha) {
-		
-		int count=0;
-		
-		String[] cro = {"c=","c-","dz=","d-","lj","nj","s=","z="};	
-		/*
-		if(alpha.contains("c=")) { count++; }
-		if(alpha.contains("c-")) { count++; }
-		if(alpha.contains("dz=")) { count++; }
-		if(alpha.contains("d-")) { count++; }
-		if(alpha.contains("lj")) { count++; }
-		if(alpha.contains("nj")) { count++; }
-		if(alpha.contains("s=")) { count++; }
-		if(alpha.contains("z=")) { count++; }
-		*/
-		
-		for(int i=0; i<alpha.length(); i++) {
-			
-			
-		}
-		
-		return count;
-		
-	}
 
-}
+		int len = alpha.length();
+		int count=0;
+		for(int i=0; i<alpha.length(); i++) {
+			try {
+				// c ~
+				if(alpha.charAt(i) == 'c') {
+					if(alpha.charAt(i+1) == '=') {
+						count++;
+					}
+					else if(alpha.charAt(i+1) == '-') {
+						count++;
+					}
+				}
+				
+				// d ~
+				else if(alpha.charAt(i) == 'd') {
+					if(alpha.charAt(i+1) == 'z') {
+						if(alpha.charAt(i+2) == '=') {
+							count++;
+						}
+					}
+					else if(alpha.charAt(i+1) == '-') {
+						count++;
+					}
+				}
+				
+				// lj
+				else if(alpha.charAt(i) == 'l') {
+					if(alpha.charAt(i+1) == 'j') {
+						count++;
+					}
+				}
+				
+				// nj
+				else if(alpha.charAt(i) == 'n') {
+					if(alpha.charAt(i+1) == 'j') {
+						count++;
+					}
+				}
+				
+				// s=
+				else if(alpha.charAt(i) == 's') {
+					if(alpha.charAt(i+1) == '=') {
+						count++;
+					}
+				}
+				// z=
+				else if(alpha.charAt(i) == 'z') {
+					if(alpha.charAt(i+1) == '=') {
+						count++;
+					}
+				}
+			}catch(Exception e) {
+				break;
+				}
+			}
+		return len-count;
+		}
+	}
