cfb992fceade9e39622654ab00683910c1cd1a2d
DongJun Kim <dj991108@naver.com>
2022-08-27 01:22:28 +0900

    Greedy

--- src/silver/L2_1541.java
+++ src/silver/L2_1541.java
@@ -1,15 +1,13 @@
 package silver;
 
 import java.util.Scanner;
-
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptEngine;
+import java.util.Stack;
 import javax.script.ScriptException;
 import java.util.List;
 import java.util.ArrayList;
 
 public class L2_1541 {
-
+	
 	public static void main(String[] args)throws ScriptException {
 		
 		//ArrayList로 받고 -위차 찾아서 삽입 하고 다 끝나면 그걸 한문장의 String으로 변환 한 후 작업
@@ -51,39 +49,130 @@ public class L2_1541 {
 				break;
 			}
 		}
-		/*
-		for(int i=0 ; i<fw.size(); i++) {
-			System.out.println(fw.get(i));
-		}
-		*/
+		
 		StringBuilder sb = new StringBuilder();
 		 for (Character ch: fw) {
 	            sb.append(ch);
 	        }
-		String str = sb.toString();
-	    System.out.println(str);
-	    
-	    
-	    ScriptEngineManager ss = new ScriptEngineManager();
-	    ScriptEngine engine = ss.getEngineByName("JavaScript");
-	    int num = (int)engine.eval(str);
-	    System.out.println(num);
-		
-	    
-	    
-	     /*
-		// String to Integer
-		String str = "0009";
-		int j = Integer.parseInt(str);
-		System.out.println(j);
-		
-		// 문자열 자르기
-		String strr = "ABC-DE";
-		for(int i=0;i<array.length;i++) {
-			System.out.println(array[i]);
-			}
-		*/
-		
+		String str = sb.toString();	    
+	    Calc cal = new Calc();
+	    cal.run(str);	
 	}
 
+}
+class Calc {
+    public void run(String s) {
+    	resultPrint(organize(s));
+   }
+ // ================== 중위식 문자열을 후위식 표현으로 바꿔줌 ==================
+    public String[] organize(String s) {
+    	boolean isBracket = false;
+		
+		if(s.charAt(0)=='(') {
+			isBracket=true;
+		}
+		
+		s = s.replace("(", " ( ");
+    	s = s.replace(")", " ) ");
+    	s = s.replace("+", " + ");
+    	s = s.replace("-", " - ");
+    	s = s.replace("/", " / ");
+    	s = s.replace("*", " * ");
+    	s = s.replace("  ", " ");
+    	String[] str = s.split(" ");
+    	    	
+        ArrayList<String> sb = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+        
+        
+
+        for (int i = 0; i < str.length; i++) {
+            String now = str[i];
+
+            switch (now){
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    while (!stack.isEmpty() && priority(stack.peek()) >= priority(now)) {
+                        sb.add(stack.pop());
+                    }
+                    stack.push(now);
+                    break;
+                case "(":
+                    stack.push(now);
+                    break;
+                case ")":
+                    while(!stack.isEmpty() && !stack.peek().equals("(")){                    	                              	
+                    		sb.add(stack.pop());                  	
+                    }
+                    stack.pop();
+                    break;
+                default:
+                    sb.add(now);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            sb.add(stack.pop());
+        }
+        
+        if(isBracket) {
+        	sb.remove(0);}
+        
+        String[] result = new String[sb.size()];
+
+        for(int i = 0; i < sb.size(); i++) {      
+        	result[i]=sb.get(i); 
+        }
+        
+        return result;
+    }
+    
+ // ========================== 우선순위 부여 ==========================
+    
+    public int priority(String operator){
+
+        if(operator.equals("(") || operator.equals(")")){
+            return 0;
+        } else if (operator.equals("+") || operator.equals("-")) {
+            return 1;
+        } else if (operator.equals("*") || operator.equals("/")) {
+            return 2;
+        }
+        return -1;
+    }
+ // =========================== 후위식 계산 ===========================
+    
+    public void resultPrint(String[] str) {
+    	
+    	Stack<Integer> stack = new Stack<>();
+	    
+	    for (String x : str) {
+	      
+	      if (!x.equals("+")&&!x.equals("-")&&!x.equals("*")&&!x.equals("/")) { 
+	    	  stack.push(Integer.parseInt(x));
+	      }else {
+	        
+	        int a = stack.pop();
+	        int b = stack.pop();
+	        
+	        switch (x) {
+	          case "+":
+	        	  stack.push(b+a);
+	            break;
+	          case "-":
+	        	  stack.push(b-a);
+	            break;
+	          case "*":
+	        	  stack.push(b*a);
+	            break;
+	          case "/":
+	        	  stack.push(b/a);
+	            break;
+	        }
+	      }
+	    }	
+	    System.out.println(stack.pop());
+	 }
 }
