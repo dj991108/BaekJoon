5fbbab3c09b9f7c04513f95a76fb9c4aa664894a
DongJun Kim <dj991108@naver.com>
2022-09-12 21:55:31 +0900

    Greedy

--- src/silver/L5_1789.java
+++ src/silver/L5_1789.java
@@ -7,13 +7,13 @@ public class L5_1789 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		
-		long S = s.nextLong();
+		long SS = s.nextLong();
 		long count=0;
 		long sum=0;
 		while(true) {
 			count++;
 			sum += count;
-			if(sum > S) {
+			if(sum > SS) {
 				count--;
 				break;
 			}
