3863f6d0baf69f40ffcb3026ba695e34c8622851
DongJun Kim <dj991108@naver.com>
2022-12-20 20:13:42 +0900

    Greedy

--- src/silver/L4_16435.java
+++ src/silver/L4_16435.java
@@ -6,15 +6,15 @@ import java.util.Scanner;
 public class L4_16435 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner s = new Scanner(System.in);
 
-		int N = sc.nextInt();
+		int N = s.nextInt();
 		int[] arr = new int[N];
 	
-		int snake = sc.nextInt();
+		int snake = s.nextInt();
 		
 		for(int i=0 ; i<N ; i++) {
-			arr[i] = sc.nextInt();
+			arr[i] = s.nextInt();
 		}
 		Arrays.sort(arr);
 		
