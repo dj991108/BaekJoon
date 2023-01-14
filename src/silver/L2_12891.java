bf7cfaa793e3ee50740b78d3bad0129c28582f05
DongJun Kim <dj991108@naver.com>
2023-01-14 14:42:02 +0900

    문자열

---
+++ src/silver/L2_12891.java
@@ -0,0 +1,20 @@
+package silver;
+
+import java.util.Scanner;
+
+public class L2_12891 {
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int S = s.nextInt();
+		int P = s.nextInt();
+		
+		String DNA = s.next();
+		
+		String A = s.
+
+		
+	}
+	
+
+}
